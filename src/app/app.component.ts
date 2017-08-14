f6f881c39eae75c8050d5a7e2835f0de3d231616
Oscar Lagatta <pluralsight-demo-oscar@mailinator.com>
2017-08-14 23:33:08 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { User } from './auth-form/auth-form.interface';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div>
+      <auth-form (submitted)="createUser($event)">
+        <h3>Create Account</h3>
+      </auth-form>
+      <auth-form (submitted)="loginUser($event)">
+        <h3>Login</h3>
+      </auth-form>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  createUser(user: User) {
+    console.log('Create account', user);
+  }
+  loginUser(user: User) {
+    console.log('Login', user);
+    
+  }
+
 }
