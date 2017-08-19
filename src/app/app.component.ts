e9f838a98c1b70e5b3c9d54ac4ce5a4c0fa88d0b
Oscar Lagatta <pluralsight-demo-oscar@mailinator.com>
2017-08-19 17:31:10 +0100

    ngTemplateOutlet and ngTemplateOutletContext

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,21 @@ import { AuthFormComponent } from './auth-form/auth-form.component';
 @Component({
   selector: 'app-root',
   template: `
-    <div>
-      <div #entry></div>
-
-      <template #tmpl let-name let-location="location">
+      <ng-container 
+        [ngTemplateOutlet]="tmpl"
+        [ngTemplateOutletContext]="ctx">
+      </ng-container>
+      <ng-template #tmpl let-name let-location="location">
         {{ name }}: {{ location }}
-      </template>
-    </div>
-  `,
-  styleUrls: ['./app.component.css']
+      <ng-template>
+  `
 })
-export class AppComponent implements AfterContentInit{
+export class AppComponent {
 
-  @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
-  @ViewChild('tmpl') tmpl: TemplateRef<any>;
-
-  ngAfterContentInit() {
-    // because we use template
-    this.entry.createEmbeddedView(this.tmpl, {
+    ctx = {
       $implicit: 'Oscar Lagatta',
       location: 'England, UK'
-    });
-  }
+    }
+  
 }
 
-/**
- * we create a template variable under ViewChild tml lookup
- * and create an EmbeddedView passing a particular context 
- * and in the template itself we declare an implicit context 
- * (let-name) and a property that relies on 
- *  
- */
