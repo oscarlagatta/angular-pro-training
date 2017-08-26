df654266b6f8025aeda583930d9c89f9228db0f7
Oscar Lagatta <pluralsight-demo-oscar@mailinator.com>
2017-08-26 19:40:19 +0100

    binding fromControl select

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormArray } from '@angular/forms';
+import { Product } from '../../models/product.interface';
 
 @Component({
     selector: 'stock-inventory',
@@ -14,7 +15,8 @@ import { FormControl, FormGroup, FormArray } from '@angular/forms';
                 </stock-branch>
 
                 <stock-selector 
-                    [parent]="form">
+                    [parent]="form"
+                    [products]="products">
                 </stock-selector>
 
                 <stock-products 
@@ -36,6 +38,15 @@ import { FormControl, FormGroup, FormArray } from '@angular/forms';
     `
 })
 export class StockInventoryComponent {
+
+    products: Product[] = [
+        { "id": 1, "price": 2800, "name":"MacBook Pro"},
+        { "id": 2, "price": 50, "name":"USB-C Adaptpor"},
+        { "id": 3, "price": 400, "name":"iPod"},
+        { "id": 4, "price": 900, "name":"iPhone"},
+        { "id": 5, "price": 600, "name":"Apple Watch"},
+    ];
+
     form = new FormGroup({
         store: new FormGroup({
             branch: new FormControl(''),
