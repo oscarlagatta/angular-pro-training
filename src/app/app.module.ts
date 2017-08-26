a735bdaa5e0f9e40646028ea090fa54387b2f71f
Oscar Lagatta <pluralsight-demo-oscar@mailinator.com>
2017-08-26 16:18:38 +0100

    custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CreditCardDirective } from './credit-card/credit-card.directive';
-import { ToolTipDirective } from './tooltip/tooltip.directive';
+import { FileSizePipe } from './filesize.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreditCardDirective,
-    ToolTipDirective
+    FileSizePipe
     
   ],
   imports: [
